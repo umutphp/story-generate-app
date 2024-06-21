a828213288b92253a6a6822abc031cd5422d06d1
Umut Işık <umutphp@gmail.com>
2024-06-21 17:04:34 +0300

    Add RAG base to story generation

--- core/models.py
+++ core/models.py
@@ -1,27 +1,46 @@
+import functools
+
 from django.db import models
 from django.contrib import admin
 from django.db.models import JSONField
 from django_json_widget.widgets import JSONEditorWidget
 
 
-class Story(models.Model):
-    title = models.CharField(max_length=256, null=False)
-    text = models.TextField(null=False)
-    city = models.CharField(max_length=256, null=True)
-    area = models.CharField(max_length=256, null=True)
-    accented = models.BooleanField(null=True)
-    language = models.CharField(max_length=5, default='tr')
-    generated = models.BooleanField(default=False)
-
-    def __str__(self):
-        return f"{self.title} ({self.id}) / {self.city} / {self.area}"
-
-
 class StoryTrigger(models.Model):
     language = models.CharField(max_length=5, default='tr')
     place = models.CharField(max_length=256, null=False)
     topic = models.CharField(max_length=256, null=False)
-    characters = models.JSONField()
+    setting = models.JSONField()
+
+    @property
+    def setting_str_for_prompt(self):
+        return f"* Place: {self.place}\n* Topic: {self.topic}\n* Characters: {self.characters}\n"
+
+    @property
+    def characters(self):
+        characters = ""
+
+        for char in self.setting["characters"]:
+            characters += char["kind"].lower() + " with name " + char["name"] + ", "
+
+        return characters
+
+    def __str__(self):
+        return f"{self.topic} ({self.id}) / {self.language} / {self.place}"
+
+
+class Story(models.Model):
+    title = models.CharField(max_length=256, null=False)
+    text = models.TextField(null=False)
+    city = models.CharField(max_length=256, blank=True, default='')
+    area = models.CharField(max_length=256, blank=True, default='')
+    accented = models.BooleanField(null=True)
+    language = models.CharField(max_length=5, default='tr')
+    generated = models.BooleanField(default=False)
+    story_trigger = models.ForeignKey(StoryTrigger, on_delete=models.CASCADE, default=None, null=True)
+
+    def __str__(self):
+        return f"{self.title} ({self.id}) / {self.city} / {self.area}"
 
 
 @admin.action(description="Mark selected stories as accented")
@@ -35,10 +54,10 @@ def make_not_accented(modeladmin, request, queryset):
 
 
 class StoryAdmin(admin.ModelAdmin):
-    fields = ["title", "text", "city", "area", 'accented', 'language']
-    list_display = ["title", "short_text", "language", "city", "area", 'accented']
-    list_filter = ["language", "city", "area", "accented"]
-    ordering = ["id"]
+    fields = ["title", "text", "city", "area", 'accented', 'language', 'story_trigger']
+    list_display = ["title", "short_text", "language", "story_trigger", 'accented']
+    list_filter = ["language", "accented"]
+    ordering = ["-id"]
     actions = [make_accented, make_not_accented]
 
     def short_text(self, instance):
@@ -46,10 +65,10 @@ class StoryAdmin(admin.ModelAdmin):
 
 
 class StoryTriggerAdmin(admin.ModelAdmin):
-    fields = ["topic", "place", "language", "characters"]
+    fields = ["topic", "place", "language", "setting"]
     list_display = ["topic", "place", "language"]
     list_filter = ["language"]
-    ordering = ["id"]
+    ordering = ["-id"]
 
     formfield_overrides = {
         JSONField: {'widget': JSONEditorWidget},
---
+++ story/management/commands/generate_story.py
@@ -0,0 +1,58 @@
+from django.core.management.base import BaseCommand, CommandError
+
+from core import models
+
+import ollama
+
+from langchain_core.prompts import PromptTemplate, FewShotPromptTemplate, ChatPromptTemplate, \
+    FewShotChatMessagePromptTemplate
+
+
+class Command(BaseCommand):
+    def handle(self, *args, **options):
+        prompt_prefix = """
+I want to you to create a bedtime story for kids under 6 age. Below I give you a sample story. 
+After the sample story, I will give you the information to use to generate the story. 
+I want you to write the topic in the first line and start the story after it. 
+Your respond should only contain the title and the story.
+"""
+
+        trigger = models.StoryTrigger.objects.order_by('id').first()
+        sample_stories = models.Story.objects.filter(language='en').exclude(story_trigger__isnull=True)
+        example_stories = []
+
+        for story in sample_stories:
+            example_stories.append({
+                "setting": story.story_trigger.setting_str_for_prompt,
+                "story": story.title + "\n" + story.text
+            })
+
+        example_prompt = ChatPromptTemplate.from_messages(
+            [
+                ("user", "{setting}"),
+                ("assistant", "{story}"),
+            ]
+        )
+        few_shot_prompt = FewShotChatMessagePromptTemplate(
+            example_prompt=example_prompt,
+            examples=example_stories,
+        )
+
+        final_prompt = ChatPromptTemplate.from_messages(
+            [
+                ("system", prompt_prefix),
+                few_shot_prompt,
+                ("user", "{setting}"),
+            ]
+        )
+
+        client = ollama.Client(host="http://ollama:11434")
+
+        response = client.chat(model='llama3:latest', messages=[
+            {
+                'role': 'user',
+                'content': final_prompt.format(setting=trigger.setting_str_for_prompt),
+            },
+        ])
+
+        print(f"Response: {response['message']['content']}")
